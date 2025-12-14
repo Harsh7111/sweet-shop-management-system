63fc66c532b297a4d7106ad58b74953c81bcb00a
Harsh7111 <rathodharsh097@gmail.com>
2025-12-14 21:11:16 +0530

    test: Add all 9 test cases Tests added: - test_auth.py (2 tests: register, login) - test_sweets.py (5 tests: view, add, update, delete, search) - test_inventory.py (2 tests: purchase, restock)

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -9,3 +9,15 @@ def test_register_user(client):
     )
     assert response.status_code == 200
     assert response.json()["username"] == "Harsh"
+
+
+def test_login_user(client, normal_user):
+    response = client.post(
+        "/api/auth/login",
+        json={
+            "username": "user1",
+            "password": "1234"
+        }
+    )
+    assert response.status_code == 200
+    assert "access_token" in response.json()
